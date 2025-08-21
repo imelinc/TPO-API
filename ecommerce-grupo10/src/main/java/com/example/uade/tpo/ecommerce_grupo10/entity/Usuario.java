b803bdd345709c5c019999e227f21a1f41a73b19
imelinc <ignaciomelinc@gmail.com>
2025-08-21 15:56:23 -0300

    Entidad usuario agregada

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/Usuario.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/Usuario.java
@@ -1,5 +1,110 @@
 package com.example.uade.tpo.ecommerce_grupo10.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // indica que es una entidad de JPA
+@Table(name = "Usuarios") // Le cambiamos el nombre a la tabla a "Usuarios"
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // generacion del ID automaticamente
+    private Long id;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String username;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String password;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String nombreCompleto;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String email;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String telefono;
+
+    @Column(nullable= false, length=100) // columna no puede ser nula y es un VARCHAR de 100 caracteres
+    private String direccion;
+
+    // Constructor por defecto
+    public Usuario(){}
+
+    // Constructor con parametros
+
+    public Usuario(String direccion, String email, Long id, String nombreCompleto, String password, String telefono, String username) {
+        this.direccion = direccion;
+        this.email = email;
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.password = password;
+        this.telefono = telefono;
+        this.username = username;
+    }
+
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
 }
