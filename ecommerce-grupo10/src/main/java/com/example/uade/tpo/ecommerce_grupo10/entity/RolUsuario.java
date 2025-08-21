b8924828279d20f92f1a0f26faaa17a1dd38737f
imelinc <ignaciomelinc@gmail.com>
2025-08-21 16:00:00 -0300

    Entidad rolUsuario agregada

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/RolUsuario.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/RolUsuario.java
@@ -1,5 +1,49 @@
 package com.example.uade.tpo.ecommerce_grupo10.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Roles Usuarios")
 public class RolUsuario {
     
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String nombre; // ADMIN | COMPRADOR | VENDEDOR
+
+    // Constructor por defecto
+    public RolUsuario() {}
+
+    // Constructor con parametros
+    public RolUsuario(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // --- Getters y Setters ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
 }
