6830c18b962add254c496392ab779740c5798cd0
imelinc <ignaciomelinc@gmail.com>
2025-08-21 16:11:52 -0300

    Agregue ImagenProducto.java

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/ImagenProducto.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/entity/ImagenProducto.java
@@ -1,5 +1,28 @@
 package com.example.uade.tpo.ecommerce_grupo10.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Imagenes Productos")
+@Getter // genera los getters
+@Setter // genera los setters
+@NoArgsConstructor // genera el constructor por defecto
+@AllArgsConstructor // genera el constructor con todos los parametros
 public class ImagenProducto {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, length=300)
+    private String url; // URL de la imagen del producto
 }
