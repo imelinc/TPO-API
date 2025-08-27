445beaf91ecde168f22cb98b7f162847b567015e
imelinc <ignaciomelinc@gmail.com>
2025-08-27 20:45:58 -0300

    Agregue el item carrito controller

---
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/ItemCarritoController.java
@@ -0,0 +1,29 @@
+package com.example.uade.tpo.ecommerce_grupo10.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.uade.tpo.ecommerce_grupo10.entity.__dto__.ItemCarritoDTO;
+import com.example.uade.tpo.ecommerce_grupo10.service.itemCarrito.ItemCarritoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/items-carrito")
+public class ItemCarritoController {
+
+    private final ItemCarritoService itemCarritoService;
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemCarritoDTO> obtener(@PathVariable Long itemId) {
+        return ResponseEntity.ok(itemCarritoService.obtener(itemId));
+    }
+
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long itemId) {
+        itemCarritoService.eliminar(itemId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
