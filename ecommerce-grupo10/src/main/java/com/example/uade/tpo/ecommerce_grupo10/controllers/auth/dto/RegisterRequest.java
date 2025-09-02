e4164d37761aac9017651d692f5f98800e7b929b
imelinc <ignaciomelinc@gmail.com>
2025-09-02 15:07:38 -0300

    Cree el register request

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/RegisterRequest.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/RegisterRequest.java
@@ -1,5 +1,15 @@
 package com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto;
 
+import com.example.uade.tpo.ecommerce_grupo10.entity.Rol;
+
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
-    
+    private String username;
+    private String email;
+    private String password;
+    private String nombre;
+    private String apellido;
+    private Rol rol;
 }
