51595f9dba07ee8667c3c1a64c7f720e6313467a
imelinc <ignaciomelinc@gmail.com>
2025-09-02 16:31:22 -0300

    AuthService terminado

---
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/AuthService.java
@@ -0,0 +1,77 @@
+package com.example.uade.tpo.ecommerce_grupo10.controllers.auth;
+
+import java.util.List;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.AuthRequest;
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.AuthResponse;
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.RegisterRequest;
+import com.example.uade.tpo.ecommerce_grupo10.entity.Rol;
+import com.example.uade.tpo.ecommerce_grupo10.entity.Usuario;
+import com.example.uade.tpo.ecommerce_grupo10.repository.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthResponse register(RegisterRequest req) {
+        if (usuarioRepository.existsByUsername(req.getUsername())) {
+            throw new IllegalArgumentException("El username ya está en uso");
+        }
+        if (usuarioRepository.existsByEmail(req.getEmail())) {
+            throw new IllegalArgumentException("El email ya está en uso");
+        }
+
+        Usuario u = new Usuario();
+        u.setUsername(req.getUsername());
+        u.setEmail(req.getEmail());
+        u.setPassword(passwordEncoder.encode(req.getPassword()));
+        u.setNombre(req.getNombre());
+        u.setApellido(req.getApellido());
+
+        // Si no viene rol, default COMPRADOR
+        u.setRol(req.getRol() == null ? Rol.COMPRADOR : req.getRol());
+        Usuario saved = usuarioRepository.save(u);
+
+        // creo un userDetail para el usuario registrado
+        UserDetails userDetails = new User(
+            saved.getEmail(),
+            saved.getPassword(),
+            List.of(new SimpleGrantedAuthority("ROLE_" + saved.getRol().name()))
+        );
+
+        String token = jwtService.generateToken(userDetails);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse authenticate(AuthRequest req) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(req.getEmail(), req.getPassword()));
+
+        Usuario u = usuarioRepository.findByEmailIgnoreCase(req.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Credenciales inválidas"));
+
+        // creo un userDetail para el usuario autenticado
+        UserDetails userDetails = new User(
+                u.getEmail(),
+                u.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + u.getRol().name()))
+        );
+
+        String token = jwtService.generateToken(userDetails);
+        return new AuthResponse(token);
+    }}
