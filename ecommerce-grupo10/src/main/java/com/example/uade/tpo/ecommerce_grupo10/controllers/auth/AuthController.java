ae88f59652d39ceae27477d4adb616e2144bdba7
imelinc <ignaciomelinc@gmail.com>
2025-09-02 16:55:57 -0300

    Cree el authController

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/AuthController.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/AuthController.java
@@ -1,5 +1,35 @@
 package com.example.uade.tpo.ecommerce_grupo10.controllers.auth;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.AuthRequest;
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.AuthResponse;
+import com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto.RegisterRequest;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse> authenticate(
+            @Valid @RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
 }
