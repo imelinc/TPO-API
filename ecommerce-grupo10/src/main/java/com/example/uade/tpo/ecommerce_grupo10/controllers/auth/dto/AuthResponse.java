dc1a8b6b08840809975a7b0f2eae4ff5ac03c527
imelinc <ignaciomelinc@gmail.com>
2025-09-02 14:53:15 -0300

    agregue el AuthResponse

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/AuthResponse.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/AuthResponse.java
@@ -1,5 +1,12 @@
 package com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
 public class AuthResponse {
-    
+    @JsonProperty("access_token")
+    private String token;
 }
