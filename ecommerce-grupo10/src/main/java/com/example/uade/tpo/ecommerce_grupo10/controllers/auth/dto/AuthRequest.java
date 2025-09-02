065111951ced1030333e11a37b6d3ba8986baf56
imelinc <ignaciomelinc@gmail.com>
2025-09-02 14:41:52 -0300

    Agregue el auth request

--- ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/AuthRequest.java
+++ ecommerce-grupo10/src/main/java/com/example/uade/tpo/ecommerce_grupo10/controllers/auth/dto/AuthRequest.java
@@ -1,5 +1,9 @@
 package com.example.uade.tpo.ecommerce_grupo10.controllers.auth.dto;
 
+import lombok.Data;
+
+@Data
 public class AuthRequest {
-    
+    private String email;
+    private String password;
 }
